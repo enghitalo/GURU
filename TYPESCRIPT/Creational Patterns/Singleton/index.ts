b1ca5ff69482df328d87dfd595e4544651e79ce6
Hitalo Souza <enghitalo@gmail.com>
2022-06-07 18:26:17 -0300

    refactor: Erase russian comments

--- TYPESCRIPT/Creational Patterns/Singleton/index.ts
+++ TYPESCRIPT/Creational Patterns/Singleton/index.ts
@@ -3,19 +3,11 @@
  *
  * Intent: Lets you ensure that a class has only one instance, while providing a
  * global access point to this instance.
- *
- * RU: Паттерн Одиночка
- *
- * Назначение: Гарантирует, что у класса есть только один экземпляр, и
- * предоставляет к нему глобальную точку доступа.
  */
 
 /**
  * EN: The Singleton class defines the `getInstance` method that lets clients
  * access the unique singleton instance.
- *
- * RU: Класс Одиночка предоставляет метод getInstance, который позволяет
- * клиентам получить доступ к уникальному экземпляру одиночки.
  */
 class Singleton {
     private static instance: Singleton;
@@ -23,9 +15,6 @@ class Singleton {
     /**
      * EN: The Singleton's constructor should always be private to prevent
      * direct construction calls with the `new` operator.
-     *
-     * RU: Конструктор Одиночки всегда должен быть скрытым, чтобы предотвратить
-     * создание объекта через оператор new.
      */
     private constructor() { }
 
@@ -34,11 +23,6 @@ class Singleton {
      *
      * This implementation let you subclass the Singleton class while keeping
      * just one instance of each subclass around.
-     *
-     * RU: Статический метод, управляющий доступом к экземпляру одиночки.
-     *
-     * Эта реализация позволяет вам расширять класс Одиночки, сохраняя повсюду
-     * только один экземпляр каждого подкласса.
      */
     public static getInstance(): Singleton {
         if (!Singleton.instance) {
@@ -51,9 +35,6 @@ class Singleton {
     /**
      * EN: Finally, any singleton should define some business logic, which can
      * be executed on its instance.
-     *
-     * RU: Наконец, любой одиночка должен содержать некоторую бизнес-логику,
-     * которая может быть выполнена на его экземпляре.
      */
     public someBusinessLogic() {
         // ...
@@ -62,8 +43,6 @@ class Singleton {
 
 /**
  * EN: The client code.
- *
- * RU: Клиентский код.
  */
 function clientCode() {
     const s1 = Singleton.getInstance();
